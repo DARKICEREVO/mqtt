d567334b463ae2c2dc9e0a5ba2ab007340568ce6
TanatornBig <tanatorn.nateesanprasert@gmail.com>
2019-03-10 23:30:34 +0700

    modified publisher.py

--- publisher.py
+++ publisher.py
@@ -1,34 +1,33 @@
 import socket
 import sys
+import time
+
 
 PORT = 50000
 
 
 def main():
     # format: publish 'broker_ip_address' 'topic' 'value'
-    user_input = input('>')
-    if user_input == 'quit':
-        print('Program exit')
-        sys.exit(0)
-    ip = user_input.split(' ')[1]
-    sys.stdout.flush()
-    # connect to broker
-    addr = (ip, PORT)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect(addr)
-    except:
-        print('Cannot connect to {0}'.format(addr[0]))
-        sys.exit(0)
-
-    # successfully connect to broker
     while True:
-        s.send(user_input.encode('utf-8'))
+        user_input = input('>')
         if user_input == 'quit':
             print('Program exit')
-            break
-        user_input = input('>')
+            sys.exit(0)
+        ip = user_input.split(' ')[1]
         sys.stdout.flush()
+        # connect to broker
+        addr = (ip, PORT)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            s.connect(addr)
+            s.send(user_input.encode('utf-8'))
+        except:
+            print('Cannot connect to {0}'.format(addr[0]))
+            sys.exit(0)
+        finally:
+            # send message to broker
+            s.close()
 
 
 if __name__ == '__main__':
